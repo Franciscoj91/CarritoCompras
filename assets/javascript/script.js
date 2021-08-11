56ccf9f83682e47484807e473f00fc59226b6529
Francisco Marin <franjmarin1991@gmail.com>
2021-08-11 08:48:59 -0400

    Modificacion de logica, items dibujados en html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,45 @@
 $(document).ready(function () {
 
     let id = 3896198;
+    let data = [];
+    const items = $("#item-card");
+    const templateCards = $("#template-card").contents();
+    const fragment = document.createDocumentFragment();
+    
+    
+    const fetchData = async () => {
 
+        try {
+            for (let i=0; i < 8;i++) {
+                let response = await fetch('http://www.omdbapi.com/?apikey=e0e85859&i=tt' + id);
+                data[i] = await response.json();
+                id++;
+            }
+            //console.log(data);
+            dibujarCards(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+     
+    fetchData();
 
+    const dibujarCards = data => {
+        data.forEach(producto => {
+            templateCards.find("h5").html(producto.Title);
+            templateCards.find("p").html(producto.Plot);
+            templateCards.find("img").attr("src", producto.Poster);
+            templateCards.find("button").attr("id", data.indexOf(producto));
+
+            const clon = templateCards.clone(true);
+            items.append(clon);
+        });
+        
+    }
+    
+
+    
+/* 
     for (let i = 1; i <= 8; i++) {
 
         $.ajax({
@@ -33,14 +70,20 @@ $(document).ready(function () {
                     `);
                 }
 
+                const addCarrito = e => {
+                    console.log(e.target);
+                }
+
+                $("#item-card").click(function (e) {
+                    addCarrito(e);
+                });
+
                 
-
-
 
 
             }
         });
         id++;
     }
-
+*/
 });
