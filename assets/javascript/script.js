13a8c6fd2712ab3e28b6aa94281d4da258161eef
Francisco Marin <franjmarin1991@gmail.com>
2021-08-11 12:48:03 -0400

    Objeto carrito creado

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
 
     let id = 3896198;
     let data = [];
+    let carrito = {};
     const items = $("#item-card");
     const templateCards = $("#template-card").contents();
     const fragment = document.createDocumentFragment();
     
+    
+    $("#item-card").click(e => { 
+        addCarrito(e);
+        
+    });
     
     const fetchData = async () => {
 
@@ -36,54 +42,37 @@ $(document).ready(function () {
         });
         
     }
-    
 
-    
-/* 
-    for (let i = 1; i <= 8; i++) {
-
-        $.ajax({
-
-            type: "GET",
-            url: "http://www.omdbapi.com/?apikey=e0e85859&i=tt" + id,
-            dataType: "json",
-            success: function (data) {
-
-                if (data.Response == "True") {
-                    let poster = data.Poster;
-                    let titulo = data.Title;
-                    let plot = data.Plot;
-
-                    $("#item-card").append(`
-                    <div class="col">
-                        <div class="card h-100">
-                            <img src="${poster}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${titulo}</h5>
-                                <p class="card-text">${plot}</p>
-                            </div>
-                            <div class="card-footer">
-                            <button class="btn btn-primary">Agregar al Carrito</button>
-                            </div>
-                        </div>
-                    </div>
-                    `);
-                }
-
-                const addCarrito = e => {
-                    console.log(e.target);
-                }
-
-                $("#item-card").click(function (e) {
-                    addCarrito(e);
-                });
-
-                
-
-
-            }
-        });
-        id++;
+    const addCarrito = e => {
+        let target = $(e.target);
+        //console.log(target.hasClass("btn-primary"));
+        if(target.hasClass("btn-primary")) {
+            
+            
+            setCarrito(target.parent("div"));
+        }
+        e.stopPropagation()
     }
-*/
+
+    const setCarrito = objeto => {
+        //console.log(objeto);
+        const producto = {
+            id: objeto.find(".btn-primary").attr("id"),
+            title: objeto.find("h5").text(),
+            cantidad: 1
+        }
+
+        if(carrito.hasOwnProperty(producto.id)) {
+            producto.cantidad = carrito[producto.id].cantidad + 1;
+        }
+
+        carrito[producto.id] = {...producto};
+        console.log(carrito);
+    }
+
+    
+
+
+    
+
 });
